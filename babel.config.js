f060d5462bcba34b2aca8628037231a8b776522e
TAEWOONG KANG <ktw2378@korea.ac.kr>
2022-06-27 15:58:55 +0900

    Fix: react boilerPlate type error 해결

--- babel.config.js
+++ babel.config.js
@@ -4,18 +4,9 @@ module.exports = function (api) {
     presets: ['@babel/preset-env', '@babel/preset-react'],
     plugins: [
       '@babel/plugin-transform-runtime',
-      ['import', 'syntax-dynamic-import'],
+      'babel-plugin-styled-components',
       [
-        'babel-plugin-styled-components',
-        {
-          ssr: true,
-          displayName: true,
-          preprocess: false,
-        },
-      ],
-      // ['transform-remove-console', { exclude: ['error', 'warn'] }],
-      [
-        'module-resolver',
+        require.resolve('babel-plugin-module-resolver'),
         {
           root: ['.'],
           alias: {
